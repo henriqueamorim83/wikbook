7fe32305fc7e65f910adbe142e843c7dd5733e20
julien.viet <julien.viet@ff89cc1a-a874-1a8a-b5dc-9818ba456906>
2010-04-29 11:33:42 +0000

    [maven-release-plugin]  copy for tag 0.9.2

---
+++ tags/0.9.2/core/src/main/java/org/wikbook/model/content/block/ProgramListingElement.java
@@ -0,0 +1,165 @@
+/*
+ * Copyright (C) 2010 eXo Platform SAS.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.wikbook.model.content.block;
+
+import org.wikbook.codesource.BodySource;
+import org.wikbook.codesource.CodeSourceBuilder;
+import org.wikbook.codesource.TypeSource;
+import org.wikbook.xml.ElementEmitter;
+import org.wikbook.xml.OutputFormat;
+import org.wikbook.xml.XML;
+import org.wikbook.xml.XMLEmitter;
+
+import javax.xml.transform.Transformer;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author <a href="mailto:julien.viet@exoplatform.com">Julien Viet</a>
+ * @version $Revision$
+ */
+public class ProgramListingElement extends BlockElement
+{
+
+   private static final Pattern JAVA_INCLUDE_PATTERN = Pattern.compile(
+      "\\{" + "@(include|javadoc)\\s+([^\\s]+)" + "\\}"
+   );
+
+   /** . */
+   private final String language;
+
+   /** . */
+   private final Integer indent;
+
+   /** . */
+   private final String content;
+
+   public ProgramListingElement(String language, Integer indent, String content)
+   {
+      this.language = language;
+      this.indent = indent;
+      this.content = content;
+   }
+
+   @Override
+   public void writeTo(XMLEmitter xml)
+   {
+      ElementEmitter programListingXML = xml.element("programlisting");
+      CodeLanguage codeLanguage = CodeLanguage.UNKNOWN;
+      if (language != null)
+      {
+         if ("java".equalsIgnoreCase(language))
+         {
+            codeLanguage = CodeLanguage.JAVA;
+         }
+         else if ("xml".equalsIgnoreCase(language))
+         {
+            codeLanguage = CodeLanguage.XML;
+         }
+      }
+
+      String bilto = content;
+      switch (codeLanguage)
+      {
+         case XML:
+            if (indent != null)
+            {
+               try
+               {
+                  Transformer transformer = XML.createTransformer(new OutputFormat(indent, true));
+
+                  // Output buffer
+                  StringWriter writer = new StringWriter();
+
+                  // Perform identity transformation
+                  transformer.transform(new StreamSource(new StringReader(content)), new StreamResult(writer));
+
+                  //
+                  bilto = writer.toString();
+               }
+               catch (Exception e)
+               {
+                  e.printStackTrace();
+                  bilto = "Exception occured, see logs";
+               }
+            }
+            programListingXML.content(bilto, true);
+            break;
+         case UNKNOWN:
+            programListingXML.content(bilto, true);
+            break;
+         case JAVA:
+
+            //
+            bilto = parse(bilto);
+
+            //
+            programListingXML.content(bilto, true);
+
+            //
+            break;
+      }
+   }
+
+   public static String parse(String s)
+   {
+      Matcher matcher = JAVA_INCLUDE_PATTERN.matcher(s);
+      StringBuffer sb = new StringBuffer();
+      while (matcher.find())
+      {
+
+         //
+         JavaCodeLink l = JavaCodeLink.parse(matcher.group(2));
+
+         //
+         CodeSourceBuilder builder = new CodeSourceBuilder();
+
+         //
+         TypeSource source = builder.buildClass(l.getFQN());
+
+         //
+         BodySource blah;
+         if (l.getMember() != null)
+         {
+            blah = source.findMember(l.getMember());
+         }
+         else
+         {
+            blah = source;
+         }
+
+         //
+         if ("include".equals(matcher.group(1)))
+         {
+            matcher.appendReplacement(sb, blah.getClip());
+         }
+         else if ("javadoc".equals(matcher.group(1)) && blah.getJavaDoc() != null)
+         {
+            matcher.appendReplacement(sb, blah.getJavaDoc());
+         }
+      }
+      matcher.appendTail(sb);
+      return sb.toString();
+   }
+}
