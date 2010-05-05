43ca399c7be31ee62c168bb73767822eecbab4fd
julien.viet <julien.viet@ff89cc1a-a874-1a8a-b5dc-9818ba456906>
2010-05-05 19:05:57 +0000

    [maven-release-plugin]  copy for tag 0.9.5

---
+++ tags/0.9.5/codesource/src/main/java/org/wikbook/codesource/CompilationUnitVisitor.java
@@ -0,0 +1,316 @@
+/*
+ * Copyright (C) 2010 eXo Platform SAS.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.wikbook.codesource;
+
+import japa.parser.JavaParser;
+import japa.parser.ParseException;
+import japa.parser.ast.CompilationUnit;
+import japa.parser.ast.Node;
+import japa.parser.ast.PackageDeclaration;
+import japa.parser.ast.body.BodyDeclaration;
+import japa.parser.ast.body.ClassOrInterfaceDeclaration;
+import japa.parser.ast.body.ConstructorDeclaration;
+import japa.parser.ast.body.EnumDeclaration;
+import japa.parser.ast.body.FieldDeclaration;
+import japa.parser.ast.body.JavadocComment;
+import japa.parser.ast.body.MethodDeclaration;
+import japa.parser.ast.body.VariableDeclarator;
+import japa.parser.ast.body.VariableDeclaratorId;
+import japa.parser.ast.expr.NameExpr;
+import japa.parser.ast.expr.QualifiedNameExpr;
+import japa.parser.ast.visitor.GenericVisitorAdapter;
+import org.cojen.classfile.ClassFile;
+import org.cojen.classfile.MethodDesc;
+import org.cojen.classfile.MethodInfo;
+import org.cojen.classfile.TypeDesc;
+import org.wikbook.text.Clip;
+import org.wikbook.text.Position;
+import org.wikbook.text.TextArea;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author <a href="mailto:julien.viet@exoplatform.com">Julien Viet</a>
+ * @version $Revision$
+ */
+class CompilationUnitVisitor extends GenericVisitorAdapter<Void, CompilationUnitVisitor.Visit>
+{
+
+   static class Visit
+   {
+
+      /** The current compilation unit. */
+      private final CompilationUnit compilationUnit;
+
+      /** . */
+      final LinkedList<LinkedList<BodySource>> stack = new LinkedList<LinkedList<BodySource>>();
+
+      /** . */
+      String pkg;
+
+      /** . */
+      Iterator<Signature> constructorSignatures;
+
+      /** . */
+      Iterator<Signature> methodSignatures;
+
+      /** . */
+      final List<TypeSource> types = new ArrayList<TypeSource>();
+
+      /** . */
+      private String source;
+
+      /** . */
+      private final TextArea sb;
+
+      private Visit(String source) throws ParseException
+      {
+         this.compilationUnit = JavaParser.parse(new ByteArrayInputStream(source.getBytes()));
+         this.source = source;
+         this.sb = new TextArea(source);
+      }
+
+      private String clip(Node node)
+      {
+         // Get offset of the fragment
+         int from = sb.offset(Position.get(node.getBeginLine() - 1, 0));
+         int to = sb.offset(Position.get(node.getEndLine() - 1, node.getEndColumn()));
+
+         // Get relevant chars
+         return source.substring(from, to);
+      }
+
+      private String javaDoc(BodyDeclaration node)
+      {
+         JavadocComment doc = node.getJavaDoc();
+         return doc != null ? clip(doc) : null;
+      }
+
+      private void accept(CompilationUnitVisitor visitor)
+      {
+         compilationUnit.accept(visitor, this);
+      }
+   }
+
+
+   /** . */
+   private final CodeSourceBuilder builder;
+
+   CompilationUnitVisitor(CodeSourceBuilder builder)
+   {
+      this.builder = builder;
+   }
+
+   Visit visit(String compilationUnitPath) throws IOException, ParseException, CodeSourceException
+   {
+      InputStream cuis = builder.context.getResource(compilationUnitPath);
+
+      //
+      if (cuis == null)
+      {
+         throw new CodeSourceException("Compilation path cannot be located " + compilationUnitPath);
+      }
+
+      //
+      ByteArrayOutputStream baos = new ByteArrayOutputStream();
+      byte[] buffer = new byte[20];
+      for (int l = cuis.read(buffer);l != -1;l = cuis.read(buffer))
+      {
+         baos.write(buffer, 0, l);
+      }
+      String s = baos.toString();
+
+      //
+      Visit visit = new Visit(s);
+
+      //
+      visit.accept(this);
+
+      //
+      return visit;
+   }
+
+   private String toString(NameExpr n)
+   {
+      if (n instanceof QualifiedNameExpr)
+      {
+         QualifiedNameExpr qn = (QualifiedNameExpr)n;
+         return qn.getQualifier() + "." + qn.getName();
+      }
+      else
+      {
+         return n.getName();
+      }
+   }
+
+   @Override
+   public Void visit(PackageDeclaration n, Visit arg)
+   {
+      arg.pkg = toString(n.getName());
+      return super.visit(n, arg);
+   }
+
+   @Override
+   public Void visit(ClassOrInterfaceDeclaration n, Visit v)
+   {
+      String fqn = (v.pkg.length() == 0 ? "" : (v.pkg + ".")) + n.getName();
+
+      //
+      InputStream in = builder.context.getResource(fqn.replace(".", "/") + ".class");
+      if (in == null)
+      {
+         throw new CodeSourceException("Cannot locate class file for fqn " + fqn);   
+      }
+
+      //
+      try
+      {
+
+         ClassFile cf = ClassFile.readFrom(in);
+
+         //
+         List<Signature> constructorSignatures = new ArrayList<Signature>();
+         for (MethodInfo mi : cf.getConstructors())
+         {
+            MethodDesc desc = mi.getMethodDescriptor();
+            List<String> parameterTypes = new ArrayList<String>();
+            for (TypeDesc typeDesc : desc.getParameterTypes())
+            {
+               parameterTypes.add(typeDesc.getFullName());
+            }
+            constructorSignatures.add(new Signature(parameterTypes));
+         }
+
+         //
+         List<Signature> methodSignatures = new ArrayList<Signature>();
+         for (MethodInfo mi : cf.getMethods())
+         {
+            MethodDesc desc = mi.getMethodDescriptor();
+            List<String> parameterTypes = new ArrayList<String>();
+            for (TypeDesc typeDesc : desc.getParameterTypes())
+            {
+               parameterTypes.add(typeDesc.getFullName());
+            }
+            methodSignatures.add(new Signature(parameterTypes));
+         }
+
+         //
+         v.constructorSignatures = constructorSignatures.iterator();
+         v.methodSignatures = methodSignatures.iterator();
+      }
+      catch (IOException e)
+      {
+         throw new CodeSourceException("Could not load class " + fqn, e);
+      }
+
+      //
+      v.stack.addLast(new LinkedList<BodySource>());  
+      Void ret = super.visit(n, v);
+      List<BodySource> blah = v.stack.removeLast();
+
+      //
+      Clip typeClip = Clip.get(n.getBeginLine() - 1, 0, n.getEndLine() - 1, n.getEndColumn());
+
+      //
+      TypeSource typeSource = new TypeSource(
+         new TextArea(v.source),
+         fqn,
+         typeClip,
+         v.javaDoc(n));
+
+      //
+      for (BodySource bodySource : blah)
+      {
+         if (bodySource instanceof MethodSource)
+         {
+            MethodSource methodSource = (MethodSource)bodySource;
+            typeSource.addMethod(methodSource);
+         }
+         else if (bodySource instanceof FieldSource)
+         {
+            FieldSource fieldSource = (FieldSource)bodySource;
+            typeSource.addField(fieldSource);
+         }
+      }
+
+      //
+      v.types.add(typeSource);
+
+      //
+      return ret;
+   }
+
+   @Override
+   public Void visit(EnumDeclaration n, Visit v)
+   {
+      return super.visit(n, v);
+   }
+
+   @Override
+   public Void visit(ConstructorDeclaration n, Visit v)
+   {
+      Signature signature = v.constructorSignatures.next();
+      MethodSource methodSource = new MethodSource(
+         new MemberKey(n.getName(), signature),
+         Clip.get(n.getBeginLine() - 1, 0, n.getEndLine() - 1, n.getEndColumn()),
+         v.javaDoc(n));
+      v.stack.getLast().addLast(methodSource);
+      return super.visit(n, v);
+   }
+
+   @Override
+   public Void visit(FieldDeclaration n, Visit v)
+   {
+      if (n.getVariables().size() == 1)
+      {
+         VariableDeclarator declarator = n.getVariables().get(0);
+         VariableDeclaratorId id = declarator.getId();
+         FieldSource fieldSource = new FieldSource(
+            id.getName(),
+            Clip.get(n.getBeginLine() - 1, 0, n.getEndLine() - 1, n.getEndColumn()),
+            v.javaDoc(n));
+         v.stack.getLast().addLast(fieldSource);
+      }
+      else
+      {
+         throw new UnsupportedOperationException("todo");
+      }
+      return super.visit(n, v);
+   }
+
+   @Override
+   public Void visit(MethodDeclaration n, Visit v)
+   {
+      Signature signature = v.methodSignatures.next();
+      MethodSource methodSource = new MethodSource(
+         new MemberKey(n.getName(), signature),
+         Clip.get(n.getBeginLine() - 1, 0, n.getEndLine() - 1, n.getEndColumn()),
+         v.javaDoc(n));
+      v.stack.getLast().addLast(methodSource);
+      return super.visit(n, v);
+   }
+}
---
+++ tags/0.9.5/core/src/main/java/org/wikbook/model/content/block/ProgramListingElement.java
@@ -0,0 +1,287 @@
+/*
+ * Copyright (C) 2010 eXo Platform SAS.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.wikbook.model.content.block;
+
+import org.wikbook.ResourceType;
+import org.wikbook.WikletContext;
+import org.wikbook.codesource.BodySource;
+import org.wikbook.codesource.CodeSourceBuilder;
+import org.wikbook.codesource.CodeSourceBuilderContext;
+import org.wikbook.codesource.TypeSource;
+import org.wikbook.text.TextArea;
+import org.wikbook.xml.ElementEmitter;
+import org.wikbook.xml.OutputFormat;
+import org.wikbook.xml.XML;
+import org.wikbook.xml.XMLEmitter;
+
+import javax.xml.transform.Transformer;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.net.URL;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.TreeMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author <a href="mailto:julien.viet@exoplatform.com">Julien Viet</a>
+ * @version $Revision$
+ */
+public class ProgramListingElement extends BlockElement
+{
+
+   /** . */
+   private static final String WHITE_NON_CR = "[ \t\\x0B\f\r]";
+
+   /** . */
+   private static final Pattern LINE_COMMENT = Pattern.compile("//\\s*<([0-9]+)>" + WHITE_NON_CR + "*(=)?([^\n]*)");
+
+   /** . */
+   private static final Pattern JAVA_INCLUDE_PATTERN = Pattern.compile(
+      "\\{" + "@(include|javadoc)\\s+([^\\s]+)" + "\\s*\\}"
+   );
+
+   /** . */
+   private static final Random random = new Random();
+
+   /** . */
+   private final String language;
+
+   /** . */
+   private final Integer indent;
+
+   /** . */
+   private final String content;
+
+   /** . */
+   private final WikletContext context;
+
+   public ProgramListingElement(WikletContext context, String language, Integer indent, String content)
+   {
+      this.context = context;
+      this.language = language;
+      this.indent = indent;
+      this.content = content;
+   }
+
+   @Override
+   public void writeTo(XMLEmitter xml)
+   {
+      ElementEmitter programListingCoXML = xml.element("programlistingco");
+      ElementEmitter programListingXML = programListingCoXML.element("programlisting");
+      CodeLanguage codeLanguage = CodeLanguage.UNKNOWN;
+      if (language != null)
+      {
+         if ("java".equalsIgnoreCase(language))
+         {
+            codeLanguage = CodeLanguage.JAVA;
+         }
+         else if ("xml".equalsIgnoreCase(language))
+         {
+            codeLanguage = CodeLanguage.XML;
+         }
+      }
+
+      String bilto = content;
+      switch (codeLanguage)
+      {
+         case XML:
+            if (indent != null)
+            {
+               try
+               {
+                  Transformer transformer = XML.createTransformer(new OutputFormat(indent, true));
+
+                  // Output buffer
+                  StringWriter writer = new StringWriter();
+
+                  // Perform identity transformation
+                  transformer.transform(new StreamSource(new StringReader(content)), new StreamResult(writer));
+
+                  //
+                  bilto = writer.toString();
+               }
+               catch (Exception e)
+               {
+                  e.printStackTrace();
+                  bilto = "Exception occured, see logs";
+               }
+            }
+            programListingXML.content(bilto, true);
+            break;
+         case UNKNOWN:
+            programListingXML.content(bilto, true);
+            break;
+         case JAVA:
+
+            // TreeMap will sort by callout id
+            TreeMap<String, Callout> callouts = new TreeMap<String, Callout>();
+
+            //
+            parse(bilto, programListingXML, callouts);
+
+            //
+            if (callouts.size() > 0)
+            {
+               ElementEmitter calloutListXML = programListingCoXML.element("calloutlist");
+               for (Map.Entry<String, Callout> callout : callouts.entrySet())
+               {
+                  if (callout.getValue().text != null)
+                  {
+                     StringBuffer sb = new StringBuffer();
+                     for (String coId : callout.getValue().ids)
+                     {
+                        if (sb.length() > 0)
+                        {
+                           sb.append(" ");
+                        }
+                        sb.append(coId);
+                     }
+                     calloutListXML.element("callout").withAttribute("arearefs", sb.toString()).element("para").content(callout.getValue().text, true);
+                  }
+               }
+            }
+
+            //
+            break;
+      }
+   }
+
+   private void printJavaSource(String s, XMLEmitter programListingElt, Map<String, Callout> callouts)
+   {
+      TextArea ta = new TextArea(s);
+      Random random = new Random();
+
+      Matcher matcher = LINE_COMMENT.matcher(s);
+      int prev = 0;
+      while (matcher.find())
+      {
+         String id = matcher.group(1);
+
+         //
+         programListingElt.content(ta.clip(ta.position(prev), ta.position(matcher.start())), true);
+
+         //
+         Callout callout = callouts.get(id);
+         if (callout == null)
+         {
+            callout = new Callout();
+            callouts.put(id, callout);
+         }
+
+         //
+         if (!"=".equals(matcher.group(2)))
+         {
+            String coId = "" + Math.abs(random.nextLong());
+            callout.ids.add(coId);
+            programListingElt.element("co").withAttribute("id", coId);
+         }
+
+         // Determine if we have callout text associated
+         String text = matcher.group(3);
+         if (!text.matches("\\s*"))
+         {
+            callout.text = text.trim();
+         }
+
+         // Iterate to next
+         prev = matcher.end();
+      }
+      programListingElt.content(ta.clip(ta.position(prev)), true);
+   }
+
+   private void parse(String s, XMLEmitter programListingElt, Map<String, Callout> callouts)
+   {
+      int prev = 0;
+      Matcher matcher = JAVA_INCLUDE_PATTERN.matcher(s);
+      while (matcher.find())
+      {
+         JavaCodeLink l = JavaCodeLink.parse(matcher.group(2));
+         CodeSourceBuilder builder = new CodeSourceBuilder(new CodeSourceBuilderContext()
+         {
+            public InputStream getResource(String path)
+            {
+               try
+               {
+                  List<URL> list = context.resolveResources(ResourceType.JAVA, path);
+                  if (list.size() > 0)
+                  {
+                     return list.get(0).openStream();
+                  }
+               }
+               catch (IOException e)
+               {
+                  e.printStackTrace();
+               }
+               return null;
+            }
+         });
+         TypeSource typeSource = builder.buildClass(l.getFQN());
+         BodySource source;
+         if (l.getMember() != null)
+         {
+            source = typeSource.findMember(l.getMember());
+         }
+         else
+         {
+            source = typeSource;
+         }
+
+         //
+         printJavaSource(s.substring(prev, matcher.start()), programListingElt, callouts);
+
+         //
+         if ("include".equals(matcher.group(1)))
+         {
+            printJavaSource(source.getClip(), programListingElt, callouts);
+         }
+         else if ("javadoc".equals(matcher.group(1)) && source.getJavaDoc() != null)
+         {
+            String javadoc = source.getJavaDoc();
+            programListingElt.content(javadoc, true);
+         }
+
+         //
+         prev = matcher.end();
+      }
+
+      //
+      printJavaSource(s.substring(prev), programListingElt, callouts);
+   }
+
+   private static class Callout
+   {
+
+      /** . */
+      private String text;
+
+      /** . */
+      private final LinkedList<String> ids = new LinkedList<String>();
+
+   }
+
+}
