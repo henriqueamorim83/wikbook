3854fc0cdc212f01577f5afd5e5f5edec9f1db08
julien.viet <julien.viet@ff89cc1a-a874-1a8a-b5dc-9818ba456906>
2010-05-21 09:28:00 +0000

    [maven-release-plugin]  copy for tag 0.9.16

---
+++ tags/0.9.16/core/src/test/java/org/wikbook/core/codesource/TestCodeContext.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (C) 2010 eXo Platform SAS.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.wikbook.core.codesource;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author <a href="mailto:julien.viet@exoplatform.com">Julien Viet</a>
+ * @version $Revision$
+ */
+public class TestCodeContext implements CodeContext
+{
+
+   /** . */
+   private final StringBuilder buffer = new StringBuilder();
+
+   public void writeContent(String content)
+   {
+      buffer.append(content);
+   }
+
+   public void writeCallout(String id)
+   {
+      buffer.append("// <").append(id).append(">");
+   }
+
+   public void setCallout(String id, String text)
+   {
+
+   }
+
+   public InputStream resolveResources(String id) throws IOException
+   {
+      return Thread.currentThread().getContextClassLoader().getResourceAsStream(id);
+   }
+
+   public String getText()
+   {
+      return buffer.toString();
+   }
+}
