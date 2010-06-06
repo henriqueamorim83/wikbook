9770e73fd63b76fa0372f8ef533174a2b21a2c02
julien.viet <julien.viet@ff89cc1a-a874-1a8a-b5dc-9818ba456906>
2010-06-06 14:53:48 +0000

    [maven-release-plugin]  copy for tag 0.9.20

---
+++ tags/0.9.20/codesource/src/test/java/org/wikbook/codesource/Foo.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2010 eXo Platform SAS.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.wikbook.codesource;
+
+/**
+ * @author <a href="mailto:julien.viet@exoplatform.com">Julien Viet</a>
+ * @version $Revision$
+ */
+public class Foo
+{
+
+   /**
+    * The juu String.
+    */
+   private String juu = null;
+
+   /**
+    * Empty constructor.
+    */
+   public Foo()
+   {
+   }
+
+   public void bar(String s)
+   {
+   }
+
+   /**
+    * Bar method.
+    */
+   public void bar()
+   {
+   }
+
+   public void bar(byte[] bytes)
+   {
+   }
+
+}
