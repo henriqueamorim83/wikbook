c4d828f4bc79adab67f834a0fc1a19fe98633fef
julien.viet <julien.viet@ff89cc1a-a874-1a8a-b5dc-9818ba456906>
2010-08-19 23:14:41 +0000

    [maven-release-plugin]  copy for tag 0.9.27

---
+++ tags/0.9.27/doc/src/main/java/org/wikbook/doc/AnObject.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2010 eXo Platform SAS.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.wikbook.doc;
+
+/**
+ * @author <a href="mailto:julien.viet@exoplatform.com">Julien Viet</a>
+ * @version $Revision$
+ */
+public class AnObject
+{
+   public void foo()
+   {
+      // -1-
+      String a1 = "the first";
+      String b1 = "block";
+      String c1 = "of code";
+
+      // -2-
+      String a2 = "the second";
+      String b2 = "block";
+      String c2 = "of code";
+
+      // -1-
+      String a3 = "the third";
+      String b3 = "block";
+      String c3 = "of code";
+   }
+
+   public void foo(String s)
+   {
+      // An empty method
+   }
+}
